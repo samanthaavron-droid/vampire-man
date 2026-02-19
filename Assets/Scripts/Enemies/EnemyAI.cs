681687806b3c5d85911f49b1faf43f80804d603f
ReganUA <162838412+ReganUA@users.noreply.github.com>
2026-02-19 13:46:56 +0100

    EnemyAIRaw

--- Assets/Scripts/Enemies/EnemyAI.cs
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -3,26 +3,27 @@ using UnityEngine;
 
 public class EnemyAI : Seeker
 {
-    private void Update()
-    {
-        if (path != null)
-        {
-            FollowPath();
-        }
-    }
+    //private void Update()
+    //{
+    //    if (path != null)
+    //    {
+    //        FollowPath();
+    //    }
+    //}
 
-    void FollowPath()
-    {
-        if (targetIndex >= path.Count) return;
+    //void FollowPath()
+    //{
+    //    if (targetIndex >= path.Count) return;
 
-        Vector3 targetPos = path[targetIndex].Position;
+    //    Vector3 targetPos = path[targetIndex].Position;
 
-        // Рухаємося
+    //    // Рухаємося
+    //    MovementSys.Move(gameObject, targetPos, speed);
 
-        // Використовуємо sqrMagnitude для оптимізації (0.1f * 0.1f = 0.01f)
-        if ((transform.position - targetPos).sqrMagnitude < 0.01f)
-        {
-            targetIndex++;
-        }
-    }
+    //    // Використовуємо sqrMagnitude для оптимізації (0.1f * 0.1f = 0.01f)
+    //    if ((transform.position - targetPos).sqrMagnitude < 0.01f)
+    //    {
+    //        targetIndex++;
+    //    }
+    //}
 }
